4e6f259714728784da90209c4f0d5b356a91c0d2
unknown <darkhan.zhetkergen@mail.ru>
2019-02-02 10:50:38 +0600

    Week 1,task 5

---
+++ Week 1/Task 2/Task 2/ConsoleApp1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            public string name;
+        public string id;
+        public int year ;
+        public ConsoleApp1(string name,string id,int year)
+        {
+            this.name = name;
+            this.id = id;
+            this.year = year;
+            Console.WriteLine(this.name +""this.id""+this.year"");
+        }
+        }
+    }
+}
---
+++ Week 1/Task 2/Task 2/Task 2/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_2
+{
+    class Student
+    {
+        public string name;
+        public string id;
+        public int year;
+        
+        public Student(string name,string id,int year)
+        {
+            this.name = name;
+            this.id = id;
+            this.year = year;
+
+        }
+        public void print()
+        {
+            Console.WriteLine("Имя студента:  {0}",name);
+            Console.WriteLine(  "ID студента: {0}",id);
+            if (year == 1)
+            {
+                Console.WriteLine("Вы перваш!");
+            }
+            else if (year == 4)
+            {
+                Console.WriteLine("поздравляю!ты выпускник");
+            }
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите свое имя,id и год обучения:");
+            Student st1 = new Student(Console.ReadLine(), Console.ReadLine(), int.Parse(Console.ReadLine())); // Ввод данных
+        
+            st1.print();
+
+            Console.ReadLine();
+
+        }
+    }
+}
