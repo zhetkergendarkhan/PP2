d9c941b53720b624a1e14496e02f40c207b55044
unknown <darkhan.zhetkergen@mail.ru>
2019-02-15 02:22:51 +0600

    With comments

--- Week 1/Task 2/Task 2/Program.cs
+++ Week 1/Task 2/Task 2/Program.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace Task_2
 {
-    class Student
+    class Student //Создаем класс Student
     {
         public string name;
         public string id;
@@ -14,23 +14,28 @@ namespace Task_2
         
         public Student(string name,string id,int year)
         {
-            this.name = name;
-            this.id = id;
-            this.year = year;
+            this.name = name; // Назначение классу Student имени введенного пользозователем
+            this.id = id; // Назначение классу Student id введенного пользозователем
+            this.year = year; // Назначение классу Student year введенного пользозователем
 
         }
         public void print()
         {
-            Console.WriteLine("Имя студента:  {0}",name);
-            Console.WriteLine(  "ID студента: {0}",id);
+            Console.WriteLine("Имя студента:  {0}",name); // Вывод имени студента используя параметр
+            Console.WriteLine(  "ID студента: {0}",id); // Вывод id студента используя параметр
             if (year == 1)
+                
+                
             {
                 Console.WriteLine("Вы перваш!");
+                year++; // Увелечение года обучения на 1 (инкремент)
+                Console.WriteLine("В cледющим году вы будете второкурсником!");
             }
             else if (year == 4)
             {
                 Console.WriteLine("поздравляю!ты выпускник");
             }
+            
         }
     }
 
@@ -41,9 +46,9 @@ namespace Task_2
             Console.WriteLine("Введите свое имя,id и год обучения:");
             Student st1 = new Student(Console.ReadLine(), Console.ReadLine(), int.Parse(Console.ReadLine())); // Ввод данных
         
-            st1.print();
+            st1.print();// Вывод данных
 
-            Console.ReadLine();
+            Console.ReadLine(); // Терминал не закрывается))))
 
         }
     }
--- Week 1/Task 3/Task 3/Task 3/Program.cs
+++ Week 1/Task 3/Task 3/Task 3/Program.cs
@@ -10,18 +10,18 @@ namespace Task_3
     {
         static void Main(string[] args)
         {
-            int n = int.Parse(Console.ReadLine());
-            int[] arr = new int[n];
-            string[] s = Console.ReadLine().Split();
-            for(int i = 0; i < arr.Length; i++)
-            {
-                arr[i] = int.Parse(s[i]);
-            }
+            int n = int.Parse(Console.ReadLine());// Вводим n для размера массива
+            int[] arr = new int[n];// Присваиваем n и определяем размер массива
+            string[] s = Console.ReadLine().Split();// Ввод чисел через строчный массив и разделение чисел через пробел
             for (int i = 0; i < arr.Length; i++)
             {
-                Console.Write("{0} {0} ", arr[i]);
+                arr[i] = int.Parse(s[i]);// Преобразование строчного массива в числовой
             }
-            Console.ReadKey();
+            for (int i = 0; i < arr.Length; i++) // Вывод продублированных чисел
+            {
+                Console.Write("{0} {0} ", arr[i]); //параметры в помощь
+            }
+            Console.ReadKey();// Терминал не закрывается)))
         }
        
     }
