21145c33ed42ea467abb9e09bf1aede835ccc4f6
unknown <darkhan.zhetkergen@mail.ru>
2019-02-16 00:59:32 +0600

    second chance

--- Week 2/Task 2/Task 2/Task 2/Program.cs
+++ Week 2/Task 2/Task 2/Task 2/Program.cs
@@ -1,52 +1,98 @@
 ﻿using System;
+
 using System.Collections.Generic;
+
 using System.Linq;
+
 using System.Text;
+
 using System.Threading.Tasks;
+
 using System.IO;
+
+
+
 namespace Task_2
+
 {
+
     class Program
+
     {
-        static void Main(string[] args)
+
+        static bool Check(int x)// создаем функцию для проверки на prime
+
         {
-            FileStream number = new FileStream(@"C:\Users\Darkhan\Documents\PP2\Week 2\Task 2\Task 2\in.txt",FileMode.Open,FileAccess.Read);
-            StreamReader sr = new StreamReader(number);
-            FileStream prime = new FileStream(@"C:\Users\Darkhan\Documents\PP2\Week 2\Task 2\Task 2\out.txt", FileMode.Create, FileAccess.Write);
-            StreamWriter sw = new StreamWriter(prime);
-            string s = sr.ReadLine();
+
             int cnt = 0;
-            for(int i = 0; i < s.Length; i++)
-            { if(s[i]==' ')
-                {
-                    cnt++;
-                }
 
-            }
-            string[] n = s.Split(' ');
-            for(int i = 0; i < cnt + 1; i++)
+            if (x == 1) return true;
+
+            if (x == 2) return false;
+
+            for (int i = 2; i < x; i++)
+
             {
-                bool m = true;
-                int a = int.Parse(n[i]);
-                for (int j = 2; j < a; j++)
-                {
-                    if (a % j == 0)
-                    {
-                        m = false;
 
-                    }
-                }
-                if(a!=1 && m == true)
-                {
-                    sw.Write(a + " ");
-                    Console.Write(a + " ");
+                if (x % i == 0)
 
-                }
+                    cnt++;
+
             }
-            sr.Close();
-            number.Close();
-            sw.Close();
-            prime.Close();
+
+            if (cnt > 0)
+
+            {
+
+                return true;
+
+            }
+
+            else
+
+                return false;
+
         }
+
+
+
+
+        static void Main(string[] args)
+
+        {
+
+            StreamReader sr = new StreamReader(@"C:\Users\Darkhan\Documents\PP2\Week 2\Task 2\Task 2\in.txt");// считываем
+
+            StreamWriter sw = new StreamWriter(@"C:\Users\Darkhan\Documents\PP2\Week 2\Task 2\Task 2\out.txt");// записываем
+
+            string n = sr.ReadLine();
+
+            string[] number = n.Split();
+
+
+
+            foreach (string s in number)
+
+            {
+
+                if (Check(int.Parse(s)) == false)
+
+                {
+
+                    sw.Write(s + " ");
+
+                }
+
+            }
+
+            sr.Close();
+
+            sw.Close();
+
+        }
+
+
+
     }
+
 }
